938d3f4f455fcdf4321fb751dfcfeea8897f1dd0
Karol Trojanowski <trojanowski.ifuj@gmail.com>
2021-03-23 09:12:51 +0100

    Add test for PSNR, add norm to dct function

--- psnr_hvsm/dct.cpp
+++ psnr_hvsm/dct.cpp
@@ -8,7 +8,7 @@
 using xt::placeholders::_;
 
 // DCT-II performed using FFT
-xt::xtensor<double, 1> dct(const xt::xtensor<double, 1> &x)
+xt::xtensor<double, 1> dct(const xt::xtensor<double, 1> &x, bool norm)
 {
   xt::xarray<double> f = xt::zeros<double>({x.shape(0) * 4});
   auto a = xt::view(x, xt::range(_, _, -1));
@@ -19,23 +19,25 @@ xt::xtensor<double, 1> dct(const xt::xtensor<double, 1> &x)
   xt::view(fft, xt::range(1, _, 2)) *= -1.0;
 
   // normalization
-  fft(0) *= std::sqrt(1.0/(4.*x.size()));
-  xt::view(fft, xt::range(1, _)) *= std::sqrt(1.0/(2.*x.size()));
+  if(norm) {
+    fft(0) *= std::sqrt(1.0/(4.*x.size()));
+    xt::view(fft, xt::range(1, _)) *= std::sqrt(1.0/(2.*x.size()));
+  }
 
   return xt::view(fft, xt::range(_, x.shape(0)));
 }
 
 // 2D DCT-II
-xt::xtensor<double, 2> dct2(const xt::xtensor<double, 2> &x)
+xt::xtensor<double, 2> dct2(const xt::xtensor<double, 2> &x, bool norm)
 {
   xt::xtensor<double, 2> y = x;
   for (size_t j = 0; j < x.shape(1); ++j)
   {
-    xt::row(y, j) = dct(xt::row(y, j));
+    xt::row(y, j) = dct(xt::row(y, j), norm);
   }
   for (size_t i = 0; i < x.shape(0); ++i)
   {
-    xt::col(y, i) = dct(xt::col(y, i));
+    xt::col(y, i) = dct(xt::col(y, i), norm);
   }
   return y;
 }
--- psnr_hvsm/psnr_hvsm_pybind11.cpp
+++ psnr_hvsm/psnr_hvsm_pybind11.cpp
@@ -7,36 +7,56 @@
 #include "mse_hvsm.h"
 #include "psnr_hvsm.h"
 
+namespace py = pybind11;
+
 PYBIND11_MODULE(_psnr_hvsm, m)
 {
   xt::import_numpy();
 
   m.doc() = "Accelerated implementation of the PSNR-HVS and PSNR-HVS-M image metrics.";
-  m.def("dct", [](xt::pytensor<double, 1> x) -> xt::pytensor<double, 1> {
-    return xt::pytensor<double, 1>{dct(x)};
-  });
-  m.def("dct2", [](xt::pytensor<double, 2> x) -> xt::pytensor<double, 2> {
-    return xt::pytensor<double, 2>{dct2(x)};
-  });
-  m.def("hvs_mse_tile", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> double {
-    return hvs_mse_tile(x, y);
-  });
-  m.def("hvs_mse", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> xt::pytensor<double, 2> {
-    return xt::pytensor<double, 2>{hvs_mse(x, y)};
-  });
-  m.def("masking", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> double {
-    return masking(x, y);
-  });
-  m.def("hvsm_mse_tile", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> double {
-    return hvsm_mse_tile(x, y);
-  });
-  m.def("hvsm_mse", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> xt::pytensor<double, 2> {
-    return xt::pytensor<double, 2>{hvsm_mse(x, y)};
-  });
-  m.def("psnr_hvs", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> double {
-    return psnr_hvs(x, y);
-  });
-  m.def("psnr_hvsm", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> double {
-    return psnr_hvsm(x, y);
-  });
+  m.def(
+      "dct", [](xt::pytensor<double, 1> x, bool norm) -> xt::pytensor<double, 1> {
+        return xt::pytensor<double, 1>{dct(x, norm)};
+      },
+      py::arg("x"), py::arg("norm") = true);
+  m.def(
+      "dct2", [](xt::pytensor<double, 2> x, bool norm) -> xt::pytensor<double, 2> {
+        return xt::pytensor<double, 2>{dct2(x, norm)};
+      },
+      py::arg("x"), py::arg("norm") = true);
+  m.def(
+      "hvs_mse_tile", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> double {
+        return hvs_mse_tile(x, y);
+      },
+      py::arg("x"), py::arg("y"));
+  m.def(
+      "hvs_mse", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> xt::pytensor<double, 2> {
+        return xt::pytensor<double, 2>{hvs_mse(x, y)};
+      },
+      py::arg("x"), py::arg("y"));
+  m.def(
+      "masking", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> double {
+        return masking(x, y);
+      },
+      py::arg("x"), py::arg("y"));
+  m.def(
+      "hvsm_mse_tile", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> double {
+        return hvsm_mse_tile(x, y);
+      },
+      py::arg("x"), py::arg("y"));
+  m.def(
+      "hvsm_mse", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> xt::pytensor<double, 2> {
+        return xt::pytensor<double, 2>{hvsm_mse(x, y)};
+      },
+      py::arg("x"), py::arg("y"));
+  m.def(
+      "psnr_hvs", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> double {
+        return psnr_hvs(x, y);
+      },
+      py::arg("x"), py::arg("y"));
+  m.def(
+      "psnr_hvsm", [](xt::pytensor<double, 2> x, xt::pytensor<double, 2> y) -> double {
+        return psnr_hvsm(x, y);
+      },
+      py::arg("x"), py::arg("y"));
 }
