Correct calculation f HMA, add color PSNR-HA and HMA

psnr_hvsm/__main__.py
@@ -2,52 +2,75 @@ import argparse
 
 import numpy as np
 
-from . import psnr_ha_hma, psnr_hvs_hvsm
-from .bt601 import bt601luma, bt601luma_norm
+from . import psnr_ha_hma, psnr_ha_hma_color, psnr_hvs_hvsm
+from .bt601 import bt601ycbcr, bt601ypbpr
 from .psnr import psnr
 
 try:
-    import cv2
+    from imageio import imread
 except ImportError as e:
-    print('OpenCV not found. Please install psnr_hvsm with the [command_line] extra:')
+    print('imageio not found. Please install psnr_hvsm with the [command_line] extra:')
     print('$> pip install psnr_hvsm[command_line]')
     raise e
 
 
-parser = argparse.ArgumentParser(description='Compute the PSNR-HVS and PSNR-HVS-M metric between two images.')
+parser = argparse.ArgumentParser(
+    description='Compute the PSNR-HVS and PSNR-HVS-M metric between two images.')
 parser.add_argument('original', type=str, help='The original image')
 parser.add_argument('distorted', type=str, help='The distorted image')
 parser.add_argument('--json', action='store_true', help='Output in JSON')
-parser.add_argument('--bits-per-component', type=int, default=None, help='Bits per component (if image is not 8bpc)')
+parser.add_argument('--hma-luma', action='store_true',
+                    help='Force calculation of PSNR-HA and PSNR-HMA using only the luma channel even if the input is in color.')
+parser.add_argument('--max-value', type=float, default=None, help='Maximum pixel intensity value (if image is not 8bpc)')
 
 args = parser.parse_args()
 
-org = cv2.imread(args.original, cv2.IMREAD_ANYDEPTH)
-dst = cv2.imread(args.distorted, cv2.IMREAD_ANYDEPTH)
+org = imread(args.original)
+dst = imread(args.distorted)
 
-if org.dtype == np.uint8:
-    if len(org.shape) > 2:
-        org = bt601luma(org)
-        dst = bt601luma(dst)
-    else:
+if len(org.shape) > 2:
+    # discard alpha
+    if org.shape[2] > 3:
+        org = org[:, :, :3]
+    if dst.shape[2] > 3:
+        dst = dst[:, :, :3]
+
+    # color processing
+    if org.dtype == np.uint8:
+        # use BT.601 YCbCr
+        xy, xcb, xcr = bt601ycbcr(org)
+        yy, ycb, ycr = bt601ycbcr(dst)
         org = org.astype(float) / 255
         dst = dst.astype(float) / 255
+    else:
+        if args.max_value is None:
+            raise Exception('Please provide --max-value for image files that are >8bpc.')
+        org = org.astype(float) / args.max_value
+        dst = dst.astype(float) / args.max_value
+        # assuming analog values (we don't know the ITU rec. to use)
+        xy, xcb, xcr = bt601ypbpr(org)
+        yy, ycb, ycr = bt601ypbpr(dst)
+
+    if not args.hma_luma:
+        psnr_ha, psnr_hma = psnr_ha_hma_color(xy, xcb, xcr, yy, ycb, ycr)
+    else:
+        psnr_ha, psnr_hma = psnr_ha_hma(xy, yy)
+
 else:
-    if args.bits_per_component is None:
-        raise Exception('Please provide --bits-per-component for image files that are >8bpc.')
-    max_val = (1 << args.bits_per_component) - 1
-    org = org.astype(float) / max_val
-    dst = dst.astype(float) / max_val
+    if org.dtype == np.uint8:
+        xy = org.astype(float) / 255
+        yy = dst.astype(float) / 255
+    else:
+        if args.max_value is None:
+            raise Exception('Please provide --max-value for image files that are >8bpc.')
+        xy = org.astype(float) / args.max_value
+        yy = dst.astype(float) / args.max_value
 
-    if len(org.shape) > 2:
-        # simplified luma (since we don't really know which ITU rec. to use)
-        org = bt601luma_norm(org)
-        dst = bt601luma_norm(dst)
+    psnr_ha, psnr_hma = psnr_ha_hma(xy, yy)
 
-
-psnr_hvs, psnr_hvsm = psnr_hvs_hvsm(org, dst)
-psnr_ha, psnr_hma = psnr_ha_hma(org, dst)
+psnr_hvs, psnr_hvsm = psnr_hvs_hvsm(xy, yy)
 psnr_mse = psnr(org, dst)  # add PSNR for good measure
+
 
 if args.json:
     import json
@@ -59,8 +82,8 @@ if args.json:
             'psnr_hvsm': psnr_hvsm,
             'psnr_ha': psnr_ha,
             'psnr_hma': psnr_hma,
-            'psnr_y': psnr_mse
+            'psnr': psnr_mse
         }
     ))
 else:
-    print('PSNR-HVS=%f, PSNR-HVS-M=%f, PSNR-HA=%f, PSNR-HMA=%f, PSNR-Y=%f' % (psnr_hvs, psnr_hvsm, psnr_ha, psnr_hma, psnr_mse))
+    print('PSNR-HVS=%f, PSNR-HVS-M=%f, PSNR-HA=%f, PSNR-HMA=%f, PSNR=%f' % (psnr_hvs, psnr_hvsm, psnr_ha, psnr_hma, psnr_mse))

psnr_hvsm/psnr_hma.py
@@ -7,6 +7,7 @@ from . import hvs_mse, hvsm_mse
 COEF1 = 0.002
 COEF2 = 0.25
 COEF3 = 0.04
+COEF4 = 0.5
 
 
 def _get_psnr(s, mv):
@@ -16,8 +17,8 @@ def _get_psnr(s, mv):
         return min(10.0 * np.log10(mv**2/s), 100.0)
 
 
-def psnr_ha_hma(x, y):
-    """Compute the PSNR-HA and PSNR-HMA metrics for a pair of normalised single-channel images x and y."""
+def ha_hma_mse(x, y):
+    """Compute the contrast-corrected HVS and HVS-M MSE between two single-channel, normalized images."""
     xm = x.mean()
     delt = xm - y.mean()
     c = y + delt
@@ -25,7 +26,7 @@ def psnr_ha_hma(x, y):
 
     popr = np.sum((x-xm)*(c-cm))/np.sum(np.power(c-cm, 2))
 
-    d = c*popr
+    d = cm + (c-cm)*popr
 
     m1 = hvs_mse(x, c).mean()
     m2 = hvs_mse(x, d).mean()
@@ -38,7 +39,7 @@ def psnr_ha_hma(x, y):
         else:
             m1 = m2 + (m1-m2)*COEF2
 
-    m = m1 + delt**2*COEF3
+    m = m1 + (delt**2)*COEF3
 
     if n1 > n2:
         if popr < 1:
@@ -46,7 +47,29 @@ def psnr_ha_hma(x, y):
         else:
             n1 = n2 + (n1-n2)*COEF2
 
-    n = n1 + delt**2*COEF3
+    n = n1 + (delt**2)*COEF3
+
+    return m, n
+
+
+def psnr_ha_hma(x, y):
+    """Compute the PSNR-HA and PSNR-HMA metrics for a pair of normalized single-channel images x and y."""
+    m, n = ha_hma_mse(x, y)
+
+    psnr_ha = _get_psnr(m, 1)
+    psnr_hma = _get_psnr(n, 1)
+
+    return psnr_ha, psnr_hma
+
+
+def psnr_ha_hma_color(xy, xcb, xcr, yy, ycb, ycr):
+    """Compute the PSNR-HA and PSNR-HMA metrics between two RGB normalized images."""
+    my, ny = ha_hma_mse(xy, yy)
+    mcb, ncb = ha_hma_mse(xcb, ycb)
+    mcr, ncr = ha_hma_mse(xcr, ycr)
+
+    m = (my + COEF4*(mcb+mcr)) / 2
+    n = (ny + COEF4*(ncb+ncr)) / 2
 
     psnr_ha = _get_psnr(m, 1)
     psnr_hma = _get_psnr(n, 1)

tests/test_bt601.py
@@ -1,20 +1,24 @@
 import numpy as np
 from numpy.testing import assert_almost_equal
 
-from psnr_hvsm.bt601 import bt601luma, bt601luma_norm
+from psnr_hvsm.bt601 import bt601ycbcr, bt601ypbpr
 
 
-def test_bt601luma():
-    x = np.array([92, 4, 110], dtype=float).reshape(1, 1, 3)
+def test_bt601ycbcr():
+    x = np.array([110, 4, 92], dtype=float).reshape(1, 1, 3)
 
-    y = bt601luma(x).item()
+    y, cb, cr = bt601ycbcr(x)
 
-    assert(y*255 == 55)  # value generated using rgb2ycbcr in octave
+    assert(y.item() == 55/255)  # generated in octave
+    assert(cb.item() == 151/255)  # generated in octave
+    assert(cr.item() == 168/255)  # generated in octave
 
 
-def test_bt601luma_norm():
-    x = np.array([92, 4, 110], dtype=float).reshape(1, 1, 3) / 255
+def test_bt601ypbpr():
+    x = np.array([110, 4, 92], dtype=float).reshape(1, 1, 3) / 255
 
-    y = bt601luma_norm(x).item()
+    y, pb, pr = bt601ypbpr(x)
 
-    assert_almost_equal(y, 1.793176470588235e-01)  # value generated using rgb2ycbcr in octave, then renormalised to [0,1]
+    assert_almost_equal(y.item(), 1.793176470588235e-01)  # value generated using rgb2ycbcr in octave, then renormalised to [0,1]
+    assert_almost_equal(pb.item(), 6.024078254326561e-01)  # value generated using rgb2ycbcr in octave, then renormalised to [0,1]
+    assert_almost_equal(pr.item(), 6.797823837095466e-01)  # value generated using rgb2ycbcr in octave, then renormalised to [0,1]