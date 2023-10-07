c9199c8b680572d1dd762c4836df323d940892ca
Karol Trojanowski <trojanowski.ifuj@gmail.com>
2023-10-07 12:09:44 +0200

    Update cibuildwheel
    Use curl
    
    Fix curl
    
    Skip MUSL builds

--- linux-deps.sh
+++ linux-deps.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -ex
 
-yum -y install wget sudo
-wget -O Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh"
+curl -L https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh --output Miniforge3.sh
 bash Miniforge3.sh -b -p "${HOME}/.conda"
 source "${HOME}/.conda/etc/profile.d/conda.sh"
 conda activate
 conda env create -f psnr_hvsm-dev.yml -q --force
 conda activate psnr_hvsm-dev
 
-wget -O cmake.sh "https://github.com/Kitware/CMake/releases/download/v3.27.5/cmake-3.27.5-linux-x86_64.sh"
-sudo bash cmake.sh --skip-license --prefix=/usr
+curl -L https://github.com/Kitware/CMake/releases/download/v3.27.5/cmake-3.27.5-linux-x86_64.sh --output cmake.sh
+bash cmake.sh --skip-license --prefix=/usr
